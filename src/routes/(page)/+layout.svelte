054fc8b6c2a778e57b4c36e18fc8297994db5ad7
Andy <155336412+AndyRoo135@users.noreply.github.com>
2024-11-06 19:28:56 -0600

    Create +layout.svelte

---
+++ src/routes/(page)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Navbar from "./Navbar.svelte";
+  import type { LayoutData } from "./$types";
+
+  export let data: LayoutData;
+</script>
+
+<Navbar
+  loggedIn={data.loggedIn}
+  userID={data.userID}
+  nickname={data.nickname}
+  profilePhoto={data.profilePicture}
+/>
+<slot />
